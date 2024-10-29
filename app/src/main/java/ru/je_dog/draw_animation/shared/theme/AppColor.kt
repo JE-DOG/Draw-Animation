3429b45209438dfa57e3b92b9c1ac2bc368659b5
JE_DOG <gamzat.magomedov809@gmail.com>
2024-10-29 20:26:37 +0300

    Init commit

---
+++ app/src/main/java/ru/je_dog/draw_animation/shared/theme/AppColor.kt
@@ -0,0 +1,13 @@
+package ru.je_dog.draw_animation.shared.theme
+
+import androidx.compose.ui.graphics.Color
+
+object AppColor {
+    val Green = Color(0xFFA8DB10)
+    val Black = Color(0xFF000000)
+    val White = Color(0xFFFFFFFF)
+    val Blue = Color(0xFF1976D2)
+    val Red = Color(0xFFFF3D00)
+    val Gray = Color(0xFF8B8B8B)
+}
+
